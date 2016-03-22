17412b52b5696daf84113a84081da948685c9492
Kyle Hebert <kyle.hebert@gmail.com>
2016-03-22 15:21:01 -0500

    added helper method to FT, ch5p60

--- Lab8/tdd_lab/superlists/functional_tests.py
+++ Lab8/tdd_lab/superlists/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # user browses to homepage of app
         self.browser.get('http://localhost:8000')
@@ -29,13 +34,10 @@ class NewVisitorTest(unittest.TestCase):
 
         # user types a to-do into a text box
         inputbox.send_keys('Buy peacock feathers')
+
         # user press enter, and the page updates
         inputbox.send_keys(Keys.ENTER)
-
-        # page now shows the to-do as an item on the to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # textbox asks user to add another item
         # so the user adds another item
@@ -44,13 +46,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         
         # the page updates again and now shows both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows]
-                      )
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
+        
         # the site generates a unique url for the user
         # and explains that it has done so
         self.fail('Finish the test!')
