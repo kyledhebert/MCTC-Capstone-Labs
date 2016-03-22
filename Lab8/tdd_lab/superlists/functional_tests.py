e65cb41a271e0a524722b865e33e98b0d12a3a37
Kyle Hebert <kyle.hebert@gmail.com>
2016-03-22 14:59:51 -0500

    chapter 5, page 59

--- Lab8/tdd_lab/superlists/functional_tests.py
+++ Lab8/tdd_lab/superlists/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import unittest
 
 
@@ -30,20 +31,29 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         # user press enter, and the page updates
         inputbox.send_keys(Keys.ENTER)
+
         # page now shows the to-do as an item on the to-do list
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # textbox asks user to add another item
-        self.fail('Finish the test!')
+        # so the user adds another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
         # the page updates again and now shows both items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows]
+                      )
 
         # the site generates a unique url for the user
         # and explains that it has done so
-
+        self.fail('Finish the test!')
         # the user visits the unique url and sees that
         # the list is still there
 
