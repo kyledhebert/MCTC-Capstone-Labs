74c8316203a6976b7e7b195aece01fdd8c8b146f
Kyle Hebert <kyle.hebert@gmail.com>
2016-03-29 14:57:19 -0500

    unique urls for each list, one users list not visible to another, ch6p86

--- Lab8/tdd_lab/superlists/functional_tests/tests.py
+++ Lab8/tdd_lab/superlists/functional_tests/tests.py
@@ -36,8 +36,12 @@ class NewVisitorTest(LiveServerTestCase):
         # user types a to-do into a text box
         inputbox.send_keys('Buy peacock feathers')
 
-        # user press enter, and the page updates
+        # pressing enter, the user is taken to a new URL,
+        # now the page lists "1: Buy peacock feathers" as an item
+        # in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        test_user_list_url = self.browser.current_url
+        self.assertRegex(test_user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # textbox asks user to add another item
@@ -51,8 +55,34 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly')
         
-        # the site generates a unique url for the user
-        # and explains that it has done so
+        # now a new user visits the site
+        # we start a new browser session to make sure no information
+        # from the first user is coming through cookies, etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # new user visits the home page
+        # there is no sign of the first user's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # new user starts a new list by entering an item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # new user gets a unique URL
+        new_user_list_url = self.browser.current_url
+        self.assertRegex(new_user_list_url, '/lists/.+')
+        self.assertNotEqual(new_user_list_url, test_user_list_url)
+
+        # still no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
         self.fail('Finish the test!')
         # the user visits the unique url and sees that
         # the list is still there
