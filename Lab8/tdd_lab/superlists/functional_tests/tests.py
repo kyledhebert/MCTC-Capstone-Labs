f25d6389f51a2d0d3f0f6f99ae13bb14762e02be
kylehebert <kyle.hebert@gmail.com>
2016-05-25 13:28:05 -0500

    first steps of FT for layout and styling

--- Lab8/tdd_lab/superlists/functional_tests/tests.py
+++ Lab8/tdd_lab/superlists/functional_tests/tests.py
@@ -86,3 +86,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.fail('Finish the test!')
         # the user visits the unique url and sees that
         # the list is still there
+
+    def test_layout_and_styling(self):
+        # user goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # she starts a new list and sees a nicely centered input there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
